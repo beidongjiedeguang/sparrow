c1bfe3fd23e805f7af222339e3ee7209124276de
k.y <kunyuany@163.com>
2021-06-20 22:09:08 +0800

    Add rbg string

--- example.py
+++ example.py
@@ -1,5 +1,11 @@
 from sparrow.version_ops import VersionControl
+from sparrow.color_str import rgb_string, YELLOW, GREEN
 
-vc = VersionControl("sparrow-tool")
-vc.update_version(1)
+# vc = VersionControl("sparrow-tool")
+# vc.update_version(1)
 
+print(rgb_string("hello rgb string", YELLOW))
+
+if __name__ == "__main__":
+    print(rgb_string("hello rgb string", GREEN))
+    print(rgb_string("hello rgb string", [1., 1., 1.]))
