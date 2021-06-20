dd75140a177ab99a430c835f89dad828992127c0
k.y <kunyuany@163.com>
2021-06-21 00:21:11 +0800

    Some improvements

--- sparrow/version_ops.py
+++ sparrow/version_ops.py
@@ -6,32 +6,42 @@ from .string import string_add
 class VersionControl:
     def __init__(self,
                  pkgname,
+                 pkgdir,
+                 version=None,
                  filename="version-config.yaml",
-                 version="0.0.0"):
+                 ):
+
         self.config = None
-        self.pkgname = pkgname
-        self.filename = filename
-        if not os.path.exists(filename):
-            self.gen_config(pkgname, version, filename)
+        self._pkgname = pkgname
+        self._config_path = os.path.join(pkgdir, filename)
+        if version is None:
+            try:
+                self.get_config()
+            except:
+                print(f"{filename} was not exist, created now.")
+                self.gen_config('0.0.0')
         else:
-            self.config = yaml_load(filename)
+            self.gen_config(version)
 
-    def gen_config(self, name, version, filename="version-config.yaml"):
-        config = {"name": name, "version": version}
+    def gen_config(self, version="0.0.0"):
+        config = {"name": self._pkgname, "version": version}
         self.config = config
-        yaml_dump(filename, config)
+        yaml_dump(self._config_path, config)
 
-    def get_config(self, filename="version-config.yaml"):
-        config = yaml_load(filename)
+    def get_config(self):
+        config = yaml_load(self._config_path)
         self.config = config
         return config
 
     def set_version(self, version):
         self.config['version'] = version
 
+    def save_config(self):
+        yaml_dump(self._config_path, self.config)
+
     def update_version(self, version_step=1):
         self.config['version'] = string_add(self.config["version"], version_step)
-        yaml_dump(self.filename, self.config)
+        yaml_dump(self._config_path, self.config)
 
     def clean_config_file(self):
-        os.remove(self.filename)
+        os.remove(self._config_path)
