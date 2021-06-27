6f5ff5156229d406f12b9ecd07396bb1390fa6be
k.y <kunyuany@163.com>
2021-06-27 21:05:50 +0800

    Upgrade

--- example.py
+++ example.py
@@ -20,19 +20,23 @@ def function(n):
     return s
 
 if __name__ == "__main__":
-    # print(rgb_string("hello rgb string", YELLOW))
-    # print(rgb_string("hello rgb string", GREEN))
-    # print(rgb_string("hello rgb string", [1., 1., 1.]))
+    print(rgb_string("hello rgb string", YELLOW))
+    print(rgb_string("hello rgb string", GREEN))
+    print(rgb_string("hello rgb string", [1., 1., 1.]))
+    color = np.array([0.5, 0.5, 1.])
+    print(rgb_string("hello rgb string", color))
+    print(rgb_string("hello rgb string", [23, 233, 233]))
+    print(rgb_string("hello rgb string", np.array([23, 233, 233])))
     # print(function(300, 23, 432, debug=True))
-    print(function(200))
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # 声明了一个 Logger 对象
-    logger = logging.getLogger(__name__)
-
-    logger.info("Start print log")
-    logger.debug("Do something")
-    logger.warning("Something maybe fail.")
-    logger.info("Finish")
+    # print(function(200))
+    # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # # 声明了一个 Logger 对象
+    # logger = logging.getLogger(__name__)
+    #
+    # logger.info("Start print log")
+    # logger.debug("Do something")
+    # logger.warning("Something maybe fail.")
+    # logger.info("Finish")
     # print(inspect.signature(function))
     # bj = Beijing()
     # print(bj.now)
