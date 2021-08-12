3b669c22d36b0856d60c8b0cb1d823d90efe6bb6
k.y <kunyuany@163.com>
2021-08-12 12:52:55 +0800

    add try_decorator

--- sparrow/decorator.py
+++ sparrow/decorator.py
@@ -3,6 +3,44 @@ from functools import wraps, update_wrapper
 import logging
 import inspect
 import time
+import types
+
+
+class TryDecorator:
+    def __init__(self, error_return=None):
+        self.error_return = error_return
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+            except:
+                result = self.error_return
+            return result
+
+        return wrapper
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return types.MethodType(self, instance)
+
+
+def try_decorator(error_return=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                value = func(*args, **kwargs)
+            except:
+                return error_return
+            return value
+
+        return wrapper
+
+    return decorator
 
 
 def test_time(times=10):
@@ -15,32 +53,37 @@ def test_time(times=10):
             for i in range(times):
                 func(*args, **kwargs)
             end = time.time()
-            average_cost_time = (end-start)/times
+            average_cost_time = (end - start) / times
             time_str = f"{average_cost_time:.3f}"
 
             print(f"Run {rgb_string(str(times), GREEN)} times, "
                   f"the average time is {rgb_string(time_str, GREEN)} seconds.")
             return func(*args, **kwargs)
+
         return wrapper
+
     return decorate
 
 
 def runtime(func):
     """Log the runtime of the decorated function."""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         value = func(*args, **kwargs)
         end = time.time()
-        cost_time = end-start
+        cost_time = end - start
         time_str = f"{cost_time:.3f}"
         print(f"Finished {rgb_string(func.__name__, RED)} in {rgb_string(time_str, GREEN)} secs.")
         return value
+
     return wrapper
 
 
 def repeat(n=2):
     """repeat decorated function `n` times."""
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -48,7 +91,9 @@ def repeat(n=2):
             for i in range(n):
                 result = func(*args, **kwargs)
             return result
+
         return wrapper
+
     return decorator
 
 
@@ -69,22 +114,25 @@ def optional_debug(func):
         if debug:
             print(f"{func.__name__!r} returned {value!r}")
         return value
+
     sig = inspect.signature(func)
     parms = list(sig.parameters.values())
     parms.append(inspect.Parameter('debug',
-                inspect.Parameter.KEYWORD_ONLY,
-                default=debug_default))
+                                   inspect.Parameter.KEYWORD_ONLY,
+                                   default=debug_default))
     wrapper.__signature__ = sig.replace(parameters=parms)
     return wrapper
 
 
 def count_calls(func):
     """Count the number of calls made to the decorated function. """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         wrapper.num_calls += 1
         print(f"Call {wrapper.num_calls} of {func.__name__!r}")
         return func(*args, **kwargs)
+
     wrapper.num_calls = 0
     return wrapper
 
@@ -107,7 +155,7 @@ class CallReminder:
     def __init__(self, func):
         self._func = func
         self._num_calls = 0
-        
+
     def __call__(self, *args, **kwargs):
         self._num_calls += 1
         return self._func(*args, **kwargs)
@@ -124,6 +172,7 @@ def logged(level, name=None, message=None):
     log message. If name and message aren't specified,
     they default to the function's module and name.
     """
+
     def decorate(func):
         logname = name if name else func.__module__
         log = logging.getLogger(logname)
@@ -133,5 +182,7 @@ def logged(level, name=None, message=None):
         def wrapper(*args, **kwargs):
             log.log(level, logmsg)
             return func(*args, **kwargs)
+
         return wrapper
+
     return decorate
