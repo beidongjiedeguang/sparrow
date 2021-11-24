a88c1226b478a5adbcb513f0dde7ef45fe2bfa14
k.y <kunyuany@163.com>
2021-11-24 22:02:59 +0800

    Version upgrade

--- workflow.py
+++ workflow.py
@@ -2,7 +2,7 @@ from sparrow.version_ops import VersionControl
 
 pkgname = "sparrow_tool"
 pkgdir = "sparrow"
-# vc = VersionControl(pkgname, pkgdir, version="0.1.6")
+# vc = VersionControl(pkgname, pkgdir, version="0.2.3")
 vc = VersionControl(pkgname, pkgdir, version=None)
 vc.update_version()
 vc.update_readme()
