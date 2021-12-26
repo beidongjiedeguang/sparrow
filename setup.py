506e5ccd1101fb5d0f92337f65e3acb427ebb3e3
k.y <kunyuany@163.com>
2021-12-26 23:45:25 +0800

    [Upgrade] Version bump to [0.3.4]

--- setup.py
+++ setup.py
@@ -2,28 +2,12 @@
 
 from __future__ import print_function
 from setuptools import setup, find_packages
-import os
-
-
-def yaml_load(filepath):
-    from yaml import load
-    try:
-        from yaml import CLoader as Loader
-    except ImportError:
-        from yaml import Loader
-    with open(filepath, 'r', encoding="utf-8") as stream:
-        #     stream = stream.read()
-        content = load(stream, Loader=Loader)
-    return content
-
 
 pkgname = "sparrow-tool"
 pkgdir = "sparrow"
 
-version_config = yaml_load(os.path.join(pkgdir, "version-config.yaml"))
-name, version = version_config['name'], version_config['version']
 setup(
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1', 'pyyaml'],
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True,
     package_data={
         pkgdir: [
