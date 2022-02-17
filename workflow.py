fb9dbb25a8d076a37185dbc65c925007c71a00b6
k.y <kunyuany@163.com>
2022-02-17 21:38:30 +0800

    [Upgrade] Bump version (0.4.10-> 0.4.11)

--- workflow.py
+++ workflow.py
@@ -5,15 +5,18 @@ pkgname = "sparrow_tool"
 pkgdir = "sparrow"
 # vc = VersionControl(pkgname, pkgdir, version="0.2.3")
 vc = VersionControl(pkgname, pkgdir, version=None)
+old_version = vc.config['version']
+
 vc.update_version()
+new_version = vc.config['version']
 vc.update_readme()
 # os.system("black ./sparrow")
 # vc.upload_pypi()
 
 repo = Repo('.')
 repo.index.add(["README*.md", "workflow.py", 'setup.*', 'sparrow/version-config.yaml'])
-repo.index.commit(f"[Upgrade] Version bump to [{vc.config['version']}]")
-tag = f"v{vc.config['version']}"
+repo.index.commit(f"[Upgrade] Bump version ({old_version}-> {new_version})")
+tag = f"v{new_version}"
 repo.create_tag(tag)
 remote = repo.remote()
 remote.push(tag)
