a1051a7e00b6950b03ec1496e0898acbe2294c72
k.y <kunyuany@163.com>
2022-02-18 15:18:32 +0800

    Update tests

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,15 +1,28 @@
 import numpy as np
-from einops import rearrange, reduce, asnumpy, parse_shape, repeat
+from einops import rearrange, reduce, asnumpy, parse_shape
+from sparrow.transform import repeat
 
 
 def test_array():
-    c = 3
-    w = 100
-    h = 50
+    c, w, h = 3, 100, 50
     a = np.arange(c * w * h, dtype='float').reshape((c, w, h))
     assert a.shape == (c, w, h)
     assert rearrange(a, 'c w h -> w h c').shape == (w, h, c)
 
     a = reduce(a, 'c w h -> w h', 'mean')
     assert a.shape == (w, h)
-    assert repeat(a, 'w h -> w h c', c=3).shape == (w, h, 3)
+
+
+def test_repeat():
+    a1 = np.random.randn(10)
+    assert repeat(a1, 3, 0).shape == (3, 10)
+    assert repeat(a1, 3, -1).shape == (10, 3)
+
+    a2 = np.random.randn(20, 30)
+    assert repeat(a2, 3, 0).shape == (3, 20, 30)
+    assert repeat(a2, 3, -1).shape == (20, 30, 3)
+
+    a3 = np.random.randn(20, 30, 3)
+    assert repeat(a3, 10, 0).shape == (10, 20, 30, 3)
+    assert repeat(a3, 10, -1).shape == (20, 30, 3, 10)
+    assert repeat(a3, 10, 1).shape == (20, 10, 30, 3,)
