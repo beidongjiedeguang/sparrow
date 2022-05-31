17251d6c4dfc4778bcb74515853bb1a19fa8c648
kunyuan <kunyuany@163.com>
2022-05-31 16:11:10 +0800

    [Upgrade] Bump version (0.5.7 -> 0.5.8)

--- sparrow/path/core.py
+++ sparrow/path/core.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import inspect
 
 
-def rel_to_abs(rel_path: str, parents=0):
+def rel_to_abs(rel_path: str, parents=0, return_str=False, strict=False):
     """Return absolute path relative to the called file
     args:
         parent: <int> The number of times `f_back` will be calledd.
@@ -13,4 +13,10 @@ def rel_to_abs(rel_path: str, parents=0):
         for _ in range(parents):
             f = f.f_back
     current_path = Path(f.f_code.co_filename).parent
-    return current_path / rel_path
+    pathlib_path = current_path / rel_path
+    pathlib_path =  pathlib_path.resolve(strict=strict)
+    if return_str:
+        return str(pathlib_path)
+    else:
+        return pathlib_path
+
