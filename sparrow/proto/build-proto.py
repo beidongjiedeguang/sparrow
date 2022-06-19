78926a56a4de4fc338f4d4000d5566854bdb44e4
kunyuan <kunyuany@163.com>
2022-06-20 01:37:35 +0800

    add proto files

--- sparrow/proto/build-proto.py
+++ sparrow/proto/build-proto.py
@@ -3,7 +3,7 @@ from sparrow.path import rel_to_abs
 from glob import glob
 import shutil
 
-proto_dir = rel_to_abs('./proto', return_str=True)
+proto_dir = rel_to_abs('.', return_str=True)
 proto_list = [proto for proto in glob(os.path.join(proto_dir, '*.proto'))]
 
 
@@ -20,7 +20,7 @@ def build_python():
 
 def build_web():
     # https://github.com/grpc/grpc-web
-    javascript_out = os.path.join(proto_dir, 'javascript')
+    javascript_out = os.path.join(proto_dir, 'js')
     web_proto_path_list = glob(rel_to_abs('../../web/apps/*/src/proto', return_str=True))
     PROTOC_GEN_TS_PATH = os.path.join(rel_to_abs('../../web/apps/chatroom', return_str=True),
                                       "node_modules/.bin/protoc-gen-ts")
@@ -34,12 +34,14 @@ def build_web():
         {proto_path}")
 
     def gen_web__js(proto_path):
+        # 可
         os.system(f"protoc --proto_path={proto_dir} \
         --js_out=import_style=commonjs,binary:{javascript_out} \
         --grpc-web_out=import_style=commonjs,mode=grpcwebtext:{javascript_out} \
         {proto_path}")
 
     def gen_web__ts(proto_path):
+        # 可
         command = f"protoc --proto_path={proto_dir} \
         --js_out=import_style=commonjs:{javascript_out} \
         --grpc-web_out=import_style=typescript,mode=grpcweb:{javascript_out} \
@@ -53,10 +55,10 @@ def build_web():
         [shutil.copy(proto_path, web_proto_path) for web_proto_path in web_proto_path_list]
 
     # copy to web proto dir
-    # web_proto_path = rel_to_abs("../../web/apps/chatroom/src/proto/module", return_str=True)
-    # if os.path.exists(web_proto_path):
-    #     shutil.rmtree(web_proto_path)
-    # shutil.copytree(f"{javascript_out}", web_proto_path)
+    web_proto_path = rel_to_abs("../../web/apps/chatroom/src/proto/module", return_str=True)
+    if os.path.exists(web_proto_path):
+        shutil.rmtree(web_proto_path)
+    shutil.copytree(f"{javascript_out}", web_proto_path)
 
 
 build_python()
